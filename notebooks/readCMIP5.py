c1cd3b107efde679b66970899fe9bf0c349984ac
Homer Strong <homer.strong@gmail.com>
2016-08-08 12:12:26 -0700

    apply ocean grid fix to utility read function

--- notebooks/readCMIP5.py
+++ notebooks/readCMIP5.py
@@ -5,48 +5,48 @@ Function reads cmip5 data for all variables and scenarios.
 Notes
 -----
     Data : https://pcmdi.llnl.gov/search/cmip5/
-    Workshop : DIRECT-STEM Climate Workshop 
+    Workshop : DIRECT-STEM Climate Workshop
               (https://github.com/strongh/DIRECT-STEM-climate-workshop)
     Author : Zachary Labe
-    
+
 Usage
 -----
     readCMIP5Data(directory,domain,simulation,ensemble,vari)
-    
+
 """
-   
+
 def readCMIP5Data(directory,domain,simulation,ensemble,vari):
     """
-    Function reads in cmip5 data using netCDF4 module with output as an 
+    Function reads in cmip5 data using netCDF4 module with output as an
     array.
-    
+
     Parameters
     ----------
     directory : string
         working directory for cmip5 data
     domain : string
-        global, gridded 
+        global, gridded
     simulation : string
         historical, rcp26, rcp45, rcp85
     ensemble : string
         1,2,3,4,5
     vari : string
         tas,tos,pr,psl,sic,sit,snd
-        
+
     Returns
     -------
     var : 4d array
         tas,tos,pr,psl,sic,sit,snd [year,month,lat,lon]
-        
-    Usage 
+
+    Usage
     -----
     lats,lons,var = readCMIP5Data(directory,'gridded','historical','1','tas')
     """
-    
+
     ### Assign modules
     import numpy as np
     from netCDF4 import Dataset
-    
+
     ### Check for bugs in function entry
     ense = [vari=='psl',vari=='tos',vari=='sic',vari=='sit',vari=='snd']
     if any(ense) and ensemble !='1':
@@ -54,7 +54,7 @@ def readCMIP5Data(directory,domain,simulation,ensemble,vari):
         ensemble = 1
     else:
         ensemble = ensemble
-    
+
     ### Assign variable grids
     if domain == 'gridded':
         landgrid = [vari=='tas',vari=='pr',vari=='psl']
@@ -69,19 +69,17 @@ def readCMIP5Data(directory,domain,simulation,ensemble,vari):
         grid = 'global'
     else:
         ValueError('wrong value for domain selected!')
-    
-    ### Assign file    
+
+    ### Assign file
     filename = '%s_%s_CCSM4_%s_%s.nc' % (vari,grid,simulation,ensemble)
-    
+
     ### Read in netcdf file
     data = Dataset(directory + filename)
     lats = data.variables['lat'][:]
     lons = data.variables['lon'][:]
     var = data.variables['%s' % (vari)][:]
     data.close()
-    
-    print lats.shape
-    
+
     ### Select simulation
     rcps = [simulation=='rcp26',simulation=='rcp45',simulation=='rcp85']
     if simulation == 'historical':
@@ -90,7 +88,7 @@ def readCMIP5Data(directory,domain,simulation,ensemble,vari):
         years = 95
     else:
         ValueError('wrong simulation selected!')
-    ### Reshape array 
+    ### Reshape array
     landgrid = [vari=='tas',vari=='pr',vari=='psl']
     oceangrid = [vari=='tos',vari=='sic',vari=='sit',vari=='snd']
     if any(landgrid):
@@ -99,7 +97,7 @@ def readCMIP5Data(directory,domain,simulation,ensemble,vari):
         var = np.reshape(var,(years,12,lats.shape[0],lons.shape[1]))
     else:
         ValueError('%s array has not been reshaped!' % vari)
-    
+
     return lats,lons,var
 
 ###########################################################################
@@ -112,36 +110,36 @@ def readCMIP5Data(directory,domain,simulation,ensemble,vari):
 
 def readUtilityData(directory,domain,vari):
     """
-    Function reads in utility CCSM4 geographical data using netCDF4 module with output as an 
+    Function reads in utility CCSM4 geographical data using netCDF4 module with output as an
     array.
-    
+
     Parameters
     ----------
     directory : string
         working directory for cmip5 data
     domain : string
-        global, gridded 
+        global, gridded
     ensemble : string
         1,2,3,4,5
     vari : string
         areacella,areacello,landfrac,oceanfrac
-        
+
     Returns
     -------
     var : 4d array
         areacella,areacello,landfrac,oceanfrac [lat,lon]
-        
-    Usage 
+
+    Usage
     -----
     lats,lons,var = readCMIP5Data(directory,'gridded','tas')
     """
-    
+
     ### Assign modules
     import numpy as np
     from netCDF4 import Dataset
-    
 
-    
+
+
     ### Assign variable grids
     if domain == 'gridded':
         landgrid = [vari=='areacella',vari=='landfrac']
@@ -156,20 +154,27 @@ def readUtilityData(directory,domain,vari):
         grid = ''
     else:
         ValueError('wrong value for domain selected!')
-    
-    ### Assign file    
+
+    ### Assign file
     filename = '%s_%sCCSM4.nc' % (vari,grid)
-    
+
     ### Read in netcdf file
     data = Dataset(directory + filename)
     lats = data.variables['lat'][:]
     lons = data.variables['lon'][:]
     var = data.variables['%s' % (vari)][:]
     data.close()
-    
-    ### Reshape array 
-    var = np.reshape(var,(lats.shape[0],lons.shape[0]))
-    
+
+    ### Reshape array
+    landgrid = [vari=='areacella',vari=='landfrac']
+    oceangrid = [vari=='areacello',vari=='oceanfrac']
+    if any(landgrid):
+        var = np.reshape(var,(lats.shape[0],lons.shape[0]))
+    elif any(oceangrid):
+        var = np.reshape(var,(lats.shape[0],lons.shape[1]))
+    else:
+        ValueError('%s array has not been reshaped!' % vari)
+
     return lats,lons,var
 
 ###########################################################################
